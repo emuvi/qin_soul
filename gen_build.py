6c62b135067379de798b408e8928ac7eebd62c90
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-22 19:02:56 -0300

    update build scripts

--- gen_build.py
+++ gen_build.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 
 
-def generate():
-    print("Generating...")
+def install():
+    print("Installing...")
     if not os.path.isdir("node_modules"):
         os.system("npm install")
+
+
+def generate():
+    print("Generating...")
     for path in os.listdir("."):
         if path.startswith("mk_") and path.endswith(".py"):
             with open(path) as file:
--- put_test.py
+++ put_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import put_on
 
 put_on.make("qin_soul", put_on.Mode.TEST, put_on.Kind.PUB)
